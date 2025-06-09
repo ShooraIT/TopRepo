13a8b74200b216dca6b2ff0375f25efea9206c01
Student <Student@DESKTOP-6NP6TQ3>
2025-06-09 20:33:55 +0300

    Базовая версия ToDo

--- Project1/Project1.cpp
+++ Project1/Project1.cpp
@@ -1,9 +1,31 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> tasks;//массив задач строкового типа
+
+void addTask(const string& task) 
+{
+    tasks.push_back(task); //в конец масива добавляем новую задачу
+}
+
+void showTasks() 
+{
+    cout << "ToDo list:"<<endl;
+    for(size_t i=0; i<tasks.size();i++)
+    {
+        cout << i + 1 << ". " << tasks[i] << endl;
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    addTask("Go to the school");
+    addTask("Pass the exam");
+    showTasks();
+    return 0;
 }
 
 
